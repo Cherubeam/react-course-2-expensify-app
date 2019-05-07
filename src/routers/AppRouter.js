b61ef7c8747c7428c8c95e1bcca12576b7c436ef
Marco Braun <17480141+Cherubeam@users.noreply.github.com>
2019-05-07 18:02:06 +0200

    Delete HelpPage

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,7 +6,6 @@ import PrivateRoute from './PrivateRoute'
 import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
-import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFoundPage'
 
 export const history = createHistory()
@@ -19,7 +18,6 @@ const AppRouter = () => (
                 <PrivateRoute path='/dashboard' component={ExpenseDashboardPage} />
                 <PrivateRoute path='/create' component={AddExpensePage} />
                 <PrivateRoute path='/edit/:id' component={EditExpensePage} />
-                <Route path='/help' component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
